57daeb5b24e30f18217d2e0c5c65b441bb822414
Kang Hyojun <iam.kanghyojun@gmail.com>
2016-07-02 18:41:09 +0900

    Show descriptive error message

--- src/Nirum/Cli.hs
+++ src/Nirum/Cli.hs
@@ -2,6 +2,7 @@
              DeriveDataTypeable #-}
 module Nirum.Cli (main) where
 
+import qualified Data.Text as T
 import qualified Data.Text.IO as TI
 import System.Console.CmdArgs.Implicit ( Data
                                        , Typeable
@@ -20,7 +21,12 @@ import System.FilePath ( replaceDirectory
                        , replaceExtension
                        )
 import Text.InterpolatedString.Perl6 (qq)
-import Text.Megaparsec.Error (ParseError, errorMessages, showMessages)
+import Text.Megaparsec.Error ( ParseError
+                             , errorMessages
+                             , errorPos
+                             , showMessages
+                             )
+import Text.Megaparsec.Pos (SourcePos(sourceName, sourceLine, sourceColumn))
 
 import Nirum.Parser (parseFile)
 import Nirum.Targets.Python (compileModule)
@@ -32,8 +38,26 @@ data NirumCli = NirumCli { targetLanguage :: TargetLanguage
                          , destination :: FilePath
                          } deriving (Show, Data, Typeable)
 
-showError :: ParseError -> String
-showError = showMessages . errorMessages
+toErrorMessage :: ParseError -> FilePath -> IO String
+toErrorMessage parseError' filePath' = do
+    sourceCode <- readFile filePath'
+    let sourceLine' = lines sourceCode
+    return [qq|
+File: "{sourceName error'}", line $errorLine column $errorColumn
+
+{(!!) sourceLine' (errorLine - 1)}
+{arrow}
+
+{showMessages . errorMessages $ parseError'}|]
+  where
+    error' :: SourcePos
+    error' = errorPos parseError'
+    errorLine :: Int
+    errorLine = sourceLine error'
+    errorColumn :: Int
+    errorColumn = sourceColumn error'
+    arrow :: T.Text
+    arrow = T.snoc (T.concat (replicate (errorColumn - 1) (T.pack " "))) '^'
 
 nirumcli :: NirumCli
 nirumcli = NirumCli { targetLanguage = enum [ Python &= help "python" ]
@@ -52,12 +76,14 @@ main = do
         Python -> do
             result <- parseFile nirumSourceFilePath
             case result of
-                Left error' -> putStrLn [qq|error: {showError error'}|]
-                Right module' -> do
-                    let code = compileModule module'
-                        outputFilename = toOutputFilename nirumSourceFilePath $
-                            destination argument
-                    TI.writeFile outputFilename code
+              Left error' -> do
+                  m <- toErrorMessage error' nirumSourceFilePath
+                  putStrLn m
+              Right module' -> do
+                  let code = compileModule module'
+                      outputFilename = toOutputFilename nirumSourceFilePath $
+                          destination argument
+                  TI.writeFile outputFilename code
   where
     toOutputFilename :: FilePath -> FilePath -> String
     toOutputFilename source dest =
