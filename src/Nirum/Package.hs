6357eaa5a2ebb5cc39d7406d1fe4247952ae9a19
Hong Minhee <hongminhee@member.fsf.org>
2016-07-08 01:25:16 +0900

    PackageError and ImportError

--- src/Nirum/Package.hs
+++ src/Nirum/Package.hs
@@ -1,46 +1,131 @@
-module Nirum.Package ( Package(Package, modules)
+module Nirum.Package ( ImportError ( CircularImportError
+                                   , MissingImportError
+                                   , MissingModulePathError
+                                   )
+                     , Package(modules)
+                     , PackageError(ImportError, ParseError, ScanError)
+                     , makePackage
+                     , resolveModule
                      , scanModules
                      , scanPackage
                      ) where
 
-import Data.Map.Strict (Map, empty, foldrWithKey, fromList, insert)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import System.Directory (doesDirectoryExist, listDirectory)
 import System.FilePath ((</>))
 
-import Nirum.Constructs.Module (Module)
+import qualified Nirum.Constructs.DeclarationSet as DS
+import Nirum.Constructs.Identifier (Identifier)
+import qualified Nirum.Constructs.Module as Mod
 import Nirum.Constructs.ModulePath (ModulePath, fromFilePath)
+import Nirum.Constructs.TypeDeclaration ( TypeDeclaration ( Import
+                                                          , TypeDeclaration
+                                                          )
+                                        )
 import Nirum.Parser (ParseError, parseFile)
 
 -- | Represents a package which consists of modules.
-data Package = Package { modules :: Map ModulePath Module
+data Package = Package { modules :: M.Map ModulePath Mod.Module
                        } deriving (Eq, Ord, Show)
 -- TODO: uri, version, dependencies
 
+data ImportError = CircularImportError [ModulePath]
+                 | MissingModulePathError ModulePath ModulePath
+                 | MissingImportError ModulePath ModulePath Identifier
+                 deriving (Eq, Ord, Show)
+
+makePackage :: M.Map ModulePath Mod.Module -> Either (S.Set ImportError) Package
+makePackage modules'
+    | S.null importErrors = Right package
+    | otherwise = Left importErrors
+  where
+    package :: Package
+    package = Package modules'
+    importErrors :: S.Set ImportError
+    importErrors = detectImportErrors package
+
+resolveModule :: ModulePath -> Package -> Maybe Mod.Module
+resolveModule path Package { modules = ms } = M.lookup path ms
+
+detectImportErrors :: Package -> S.Set ImportError
+detectImportErrors package = detectMissingImports package `S.union`
+                             detectCircularImports package
+
+detectMissingImports :: Package -> S.Set ImportError
+detectMissingImports package@Package { modules = ms } =
+    S.fromList [e | (path, module') <- M.toList ms, e <- detect path module']
+  where
+    detect :: ModulePath -> Mod.Module -> [ImportError]
+    detect path module' =
+        [ e
+        | (path', idents) <- M.toList (Mod.imports module')
+        , e <- case resolveModule path' package of
+                Nothing -> [MissingModulePathError path path']
+                Just (Mod.Module decls _) ->
+                    [ e
+                    | i <- S.toList idents
+                    , e <- case DS.lookup i decls of
+                        Just TypeDeclaration {} -> []
+                        Just Import {} -> [MissingImportError path path' i]
+                        Nothing -> [MissingImportError path path' i]
+                    ]
+        ]
+
+detectCircularImports :: Package -> S.Set ImportError
+detectCircularImports Package { modules = ms } =
+    S.fromList [e | path <- M.keys ms, e <- detect path []]
+  where
+    moduleImports :: M.Map ModulePath (S.Set ModulePath)
+    moduleImports =
+        M.fromList [ (path, M.keysSet $ Mod.imports module')
+                   | (path, module') <- M.toList ms
+                   ]
+    detect :: ModulePath -> [ModulePath] -> [ImportError]
+    detect path reversedCycle
+        | path `elem` reversedCycle =
+            [CircularImportError $ reverse reversedCycle']
+        | otherwise =
+            case M.lookup path moduleImports of
+                Just paths -> [ e
+                              | path' <- S.toList paths
+                              , e <- detect path' reversedCycle'
+                              ]
+                Nothing -> []
+      where
+        reversedCycle' :: [ModulePath]
+        reversedCycle' = path : reversedCycle
+
+data PackageError = ScanError FilePath IOError
+                  | ParseError ModulePath ParseError
+                  | ImportError ImportError
+                  deriving (Eq, Show)
+
 -- | Scan the given package path, and then return the read package.
-scanPackage :: FilePath -> IO (Either ParseError Package)
+scanPackage :: FilePath -> IO (Either PackageError Package)
 scanPackage packagePath = do
     modulePaths <- scanModules packagePath
+    -- FIXME: catch IO errors
     modules' <- mapM parseFile modulePaths
-    return $ case foldrWithKey excludeFailedParse (Right empty) modules' of
+    return $ case M.foldrWithKey excludeFailedParse (Right M.empty) modules' of
         Right parsedModules -> Right Package { modules = parsedModules }
-        Left parseError -> Left parseError
+        Left error' -> Left error'
   where
     excludeFailedParse :: ModulePath
-                       -> Either ParseError Module
-                       -> Either ParseError (Map ModulePath Module)
-                       -> Either ParseError (Map ModulePath Module)
-    excludeFailedParse _ _ result@(Left _) = result
-    excludeFailedParse _ (Left parseError) _ = Left parseError
+                       -> Either ParseError Mod.Module
+                       -> Either PackageError (M.Map ModulePath Mod.Module)
+                       -> Either PackageError (M.Map ModulePath Mod.Module)
+    excludeFailedParse _ _ (Left error') = Left error'
+    excludeFailedParse path (Left error') _ = Left $ ParseError path error'
     excludeFailedParse path (Right module') (Right map') =
-        Right (insert path module' map')
-                 
+        Right (M.insert path module' map')
 
 -- | Scan the given path recursively, and then return the map of
 -- detected module paths.
-scanModules :: FilePath -> IO (Map ModulePath FilePath)
+scanModules :: FilePath -> IO (M.Map ModulePath FilePath)
 scanModules packagePath = do
     files <- scanFiles ""
-    return $ fromList files
+    return $ M.fromList files
   where
     isNotHidden :: FilePath -> Bool
     isNotHidden ('.':_) = False
--- test/Nirum/PackageSpec.hs
+++ test/Nirum/PackageSpec.hs
@@ -1,15 +1,117 @@
 {-# LANGUAGE OverloadedLists, OverloadedStrings #-}
 module Nirum.PackageSpec where
 
+import qualified Data.Map.Strict as M
 import System.FilePath ((</>))
 import Test.Hspec.Meta
 
-import Nirum.Package (Package(Package), scanModules, scanPackage)
+import Nirum.Constructs.Module (Module(Module))
+import Nirum.Constructs.ModulePath (ModulePath)
+import Nirum.Constructs.TypeDeclaration ( Type(Alias)
+                                        , TypeDeclaration ( Import
+                                                          , TypeDeclaration
+                                                          )
+                                        )
+import Nirum.Package ( ImportError ( CircularImportError
+                                   , MissingImportError
+                                   , MissingModulePathError
+                                   )
+                     , Package
+                     , makePackage
+                     , resolveModule
+                     , scanModules
+                     , scanPackage
+                     )
 import Nirum.Parser (parseFile)
 
+createPackage :: M.Map ModulePath Module -> Package
+createPackage modules' =
+    case makePackage modules' of
+        Right pkg -> pkg
+        Left e -> error $ "errored: " ++ show e
+
+validPackage :: Package
+validPackage =
+    createPackage [ (["foo", "bar"], Module [] $ Just "foo.bar")
+                  , (["foo", "baz"], Module [] $ Just "foo.baz")
+                  , (["foo"],        Module [] $ Just "foo")
+                  , (["qux"],        Module [] $ Just "qux")
+                  , ( ["abc"]
+                    , Module [TypeDeclaration "a" (Alias "text") Nothing]
+                             Nothing
+                    )
+                  , ( ["xyz"]
+                    , Module [ Import ["abc"] "a"
+                             , TypeDeclaration "x" (Alias "text") Nothing
+                             ] Nothing
+                    )
+                  ]
+
+missingImportsModules :: M.Map ModulePath Module
+missingImportsModules =
+    [ (["foo"], Module [ Import ["foo", "bar"] "xyz" -- MissingModulePathError
+                       , Import ["foo", "bar"] "zzz" -- MissingModulePathError
+                       , Import ["baz"] "qux"
+                       ] Nothing)
+    , (["baz"], Module [ TypeDeclaration "qux" (Alias "text") Nothing ] Nothing)
+    , (["qux"], Module [ Import ["foo"] "abc" -- MissingImportError
+                       , Import ["foo"] "def" -- MissingImportError
+                       ] Nothing)
+    ]
+
+circularImportsModules :: M.Map ModulePath Module
+circularImportsModules =
+    [ (["asdf"], Module [ Import ["asdf"] "foo"
+                        , TypeDeclaration "bar" (Alias "text") Nothing
+                        ] Nothing)
+    , (["abc", "def"], Module [Import ["abc", "ghi"] "bar"
+                              , TypeDeclaration "foo" (Alias "text") Nothing
+                              ] Nothing)
+    , (["abc", "ghi"], Module [Import ["abc", "xyz"] "baz"
+                              , TypeDeclaration "bar" (Alias "text") Nothing
+                              ] Nothing)
+    , (["abc", "xyz"], Module [Import ["abc", "def"] "foo"
+                              , TypeDeclaration "baz" (Alias "text") Nothing
+                              ] Nothing)
+    ]
+
 spec :: Spec
-spec =
+spec = do
     describe "Package" $ do
+        specify "resolveModule" $ do
+            resolveModule ["foo"] validPackage `shouldBe`
+                Just (Module [] $ Just "foo")
+            resolveModule ["foo", "bar"] validPackage `shouldBe`
+                Just (Module [] $ Just "foo.bar")
+            resolveModule ["qux"] validPackage `shouldBe`
+                Just (Module [] $ Just "qux")
+            resolveModule ["baz"] validPackage `shouldBe` Nothing
+        specify "detectMissingImports" $
+            makePackage missingImportsModules `shouldBe`
+                Left [ MissingModulePathError ["foo"] ["foo", "bar"]
+                     , MissingImportError ["qux"] ["foo"] "abc"
+                     , MissingImportError ["qux"] ["foo"] "def"
+                     ]
+        specify "detectCircularImports" $
+            makePackage circularImportsModules `shouldBe`
+                Left [ CircularImportError [["asdf"], ["asdf"]]
+                     , MissingImportError ["asdf"] ["asdf"] "foo"
+                     , CircularImportError [ ["abc", "def"]
+                                           , ["abc", "ghi"]
+                                           , ["abc", "xyz"]
+                                           , ["abc", "def"]
+                                           ]
+                     , CircularImportError [ ["abc", "ghi"]
+                                           , ["abc", "xyz"]
+                                           , ["abc", "def"]
+                                           , ["abc", "ghi"]
+                                           ]
+                     , CircularImportError [ ["abc", "xyz"]
+                                           , ["abc", "def"]
+                                           , ["abc", "ghi"]
+                                           , ["abc", "xyz"]
+                                           ]
+                     ]
         specify "scanPackage" $ do
             Right package <- scanPackage "."
             let path = "." </> "examples"
@@ -23,8 +125,8 @@ spec =
                           , (["examples", "shapes"], shapesM)
                           , (["examples", "countries"], countriesM)
                           , (["examples", "address"], addressM)
-                          ]
-            package `shouldBe` Package modules
+                          ] :: M.Map ModulePath Module
+            package `shouldBe` createPackage modules
         specify "scanModules" $ do
             let path = "." </> "examples"
             mods <- scanModules "."
