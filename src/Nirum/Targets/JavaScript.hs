795b56d9b43fe1c2a6b75ff2ad8da033b8bcdef0
Eunchong Yu <gracie@spoqa.com>
2017-03-11 11:57:39 +0900

    Add package.json output

--- src/Nirum/Targets/JavaScript.hs
+++ src/Nirum/Targets/JavaScript.hs
@@ -1,11 +1,18 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RecordWildCards, TypeFamilies #-}
 module Nirum.Targets.JavaScript ( JavaScript ) where
 
-import qualified Data.ByteString as BS
+import Data.Aeson.Encode.Pretty ( encodePrettyToTextBuilder )
+import Data.Aeson.Types ( ToJSON, (.=), object, toJSON )
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Map.Strict as M
+import Data.Map.Strict ( Map )
 import qualified Data.Text as T
+import qualified Data.Text.Lazy.Builder as LB
+import Data.Text.Lazy.Builder ( Builder, toLazyText )
+import Data.Text.Lazy.Encoding ( encodeUtf8 )
 
-import Nirum.Package.Metadata (Target ( CompileError
+import Nirum.Package.Metadata ( Package
+                              , Target ( CompileError
                                        , CompileResult
                                        , compilePackage
                                        , parseTarget
@@ -13,6 +20,7 @@ import Nirum.Package.Metadata (Target ( CompileError
                                        , targetName
                                        , toByteString
                                        )
+                              , packageTarget
                               , stringField
                               )
 
@@ -20,13 +28,28 @@ import Nirum.Package.Metadata (Target ( CompileError
 data JavaScript = JavaScript { packageName :: T.Text }
     deriving (Eq, Ord, Show)
 
+instance ToJSON JavaScript where
+    toJSON JavaScript { .. } =
+        object [ "name" .= packageName ]
+
+newtype CodeBuilder = CodeBuilder { builder :: Builder }
+
 instance Target JavaScript where
-    type CompileResult JavaScript = T.Text
+    type CompileResult JavaScript = CodeBuilder
     type CompileError JavaScript = ()
     targetName _ = "javascript"
     parseTarget table = do
         name' <- stringField "name" table
         return JavaScript { packageName = name' }
-    compilePackage _ = M.empty
+    compilePackage = compilePackage'
     showCompileError _ _e = ""
-    toByteString _ _ = BS.empty
+    toByteString _ = BSL.toStrict . encodeUtf8 . toLazyText . builder
+
+compilePackage' :: Package JavaScript -> Map FilePath (Either () CodeBuilder)
+compilePackage' package =
+    M.fromList [
+        ("package.json", Right $ compilePackageMetadata package)
+    ]
+
+compilePackageMetadata :: Package JavaScript -> CodeBuilder
+compilePackageMetadata = CodeBuilder . (`mappend` LB.singleton '\n') . encodePrettyToTextBuilder . packageTarget
