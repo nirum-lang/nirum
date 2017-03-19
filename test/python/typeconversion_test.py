6a9007f7f346b0145979f6b54ae7bbf9250710a5
Hyojun Kang <admire9@gmail.com>
2017-03-19 23:53:12 +0900

    Add testcase for type cast

---
+++ test/python/typeconversion_test.py
@@ -0,0 +1,18 @@
+from fixture.foo import Album, Name, People, Person, Song
+
+
+def test_sequence_to_tuple():
+    album = Album(name='25', tracks=[
+        Song(name='Hello')
+    ])
+    assert isinstance(album.tracks, tuple)
+    assert hash(album)
+
+
+def test_set_to_frozenset():
+    people = People(people={
+        Person(first_name=Name('hyojun'), last_name=Name('kang')),
+        Person(first_name=Name('minhee'), last_name=Name('hong'))
+    })
+    assert isinstance(people.people, frozenset)
+    assert hash(people)
