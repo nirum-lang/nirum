03f2577f997ed779698b368fb82bb23459ef7b15
Kang Hyojun <iam.kanghyojun@gmail.com>
2017-08-23 23:46:38 +0900

    Write metadata into TOML

---
+++ src/Nirum/Package/Metadata/Writer.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Nirum.Package.Metadata.Writer (encode) where
+
+import qualified Data.HashMap.Lazy as HM
+import qualified Data.SemVer as SV
+import qualified Data.Int as I
+import qualified Data.List as L
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as ENC
+import qualified Data.Time.Clock as TC
+import qualified Data.Vector as V
+import qualified Text.Email.Validate as EV
+import qualified Text.Toml.Types as TM
+
+import qualified Nirum.Package.Metadata as M
+
+renderTable :: HM.HashMap T.Text TM.Node -> T.Text
+renderTable t = T.intercalate "\n" rendered
+  where
+    rendered :: [T.Text]
+    rendered = [ renderKey k n <> renderNode n
+               | (k, n) <- L.sortOn nodePriority $ HM.toList t
+               ]
+    renderKey :: T.Text -> TM.Node -> T.Text
+    renderKey k (TM.VTable _) = "\n[" <> k <> "]\n"
+    renderKey k (TM.VTArray _) = "\n[[" <> k <> "]]\n"
+    renderKey k _ = k <> " = "
+    nodePriority :: (T.Text, TM.Node) -> Int
+    nodePriority (_, TM.VTable _) = 1
+    nodePriority (_, TM.VTArray _) = 2
+    nodePriority (_, _) = 0
+
+renderText :: T.Text -> T.Text
+renderText t = "\"" <> t <> "\""
+
+renderTableArray :: V.Vector TM.Table -> T.Text
+renderTableArray v = T.intercalate "\n" [ renderTable t | t <- V.toList v ]
+
+-- TODO
+renderInteger :: I.Int64 -> T.Text
+renderInteger _ = ""
+
+-- TODO
+renderFloat :: Double -> T.Text
+renderFloat _ = ""
+
+-- TODO
+renderBoolean :: Bool -> T.Text
+renderBoolean _ = ""
+
+-- TODO
+renderDatetime :: TC.UTCTime -> T.Text
+renderDatetime _ = ""
+
+-- TODO
+renderArray :: V.Vector TM.Node -> T.Text
+renderArray _ = ""
+
+renderNode :: TM.Node -> T.Text
+renderNode (TM.VString s) = renderText s
+renderNode (TM.VTable t) = renderTable t
+renderNode (TM.VTArray vta) = renderTableArray vta
+renderNode (TM.VInteger i) = renderInteger i
+renderNode (TM.VFloat f) = renderFloat f
+renderNode (TM.VBoolean b) = renderBoolean b
+renderNode (TM.VDatetime dt) = renderDatetime dt
+renderNode (TM.VArray a) = renderArray a
+
+encode :: M.Target t => M.Metadata t -> T.Text
+encode metadata = renderTable $ metadataToToml metadata
+
+metadataToToml :: M.Target t => M.Metadata t -> TM.Table
+metadataToToml M.Metadata { M.version = version'
+                          , M.authors = authors'
+                          , M.description = description'
+                          } =
+    HM.fromList [ ("authors", TM.VTArray $ V.fromList authors'')
+                , ("version", TM.VString $ SV.toText version')
+                , ("description", TM.VString $ fromMaybe "" description')
+                ]
+  where
+    authors'' :: [TM.Table]
+    authors'' = [ HM.fromList [ ("name", TM.VString name')
+                              , ( "email"
+                                , TM.VString $ maybe "" toEmailText email'
+                                )
+                              ]
+                | M.Author { M.name = name', M.email = email' } <- authors'
+                ]
+      where
+        toEmailText :: EV.EmailAddress -> T.Text
+        toEmailText e = ENC.decodeUtf8 $ EV.toByteString e
--- src/Nirum/Quickstart.hs
+++ src/Nirum/Quickstart.hs
@@ -1,45 +1,75 @@
-{-# LANGUAGE ExtendedDefaultRules, OverloadedStrings, QuasiQuotes #-}
+{-# LANGUAGE ExtendedDefaultRules, TypeFamilies #-}
 module Nirum.Quickstart (main) where
 
+import qualified System.Exit as SE
 import System.IO ( BufferMode (NoBuffering)
                  , hSetBuffering
                  , stdout
                  )
 
+import qualified Control.Exception as E
+import qualified Data.Map.Strict as MS
+import qualified Data.Maybe as M
 import Data.Monoid ((<>))
+import qualified Data.SemVer as SV
 import qualified Data.Text as T
-
+import qualified Data.Text.Encoding as ENC
 import qualified Data.Text.IO as TIO
 import qualified System.Process as P
-import qualified Control.Exception as E
+import qualified Text.Email.Validate as EV
+
+import Nirum.Package.Metadata ( Author ( Author
+                                       , email
+                                       , name
+                                       , uri
+                                       )
+                              , Package
+                              , Target ( CompileError
+                                       , CompileResult
+                                       , compilePackage
+                                       , parseTarget
+                                       , showCompileError
+                                       , targetName
+                                       , toByteString
+                                       )
+                              , Metadata ( Metadata
+                                          , authors
+                                          , description
+                                          , target
+                                          , version
+                                          )
+                              )
+import qualified Nirum.Package.Metadata.Writer as MW
+
+-- Dummy target to create Metadata
+data Quickstart = Quickstart deriving (Eq, Ord, Show)
+
+compilePackage' :: Package Quickstart -> MS.Map FilePath (Either () ())
+compilePackage' _ = MS.empty
+
+instance Target Quickstart where
+    type CompileResult Quickstart = ()
+    type CompileError Quickstart = ()
+    targetName _ = "quickstart"
+    parseTarget _ = return Quickstart
+    compilePackage = compilePackage'
+    showCompileError _ = const ""
+    toByteString _ = const ""
 
 defaultVersion :: T.Text
 defaultVersion = "0.0.1"
 
-main :: IO ()
-main = do
-    hSetBuffering stdout NoBuffering
-    TIO.putStr $ "version: (" <> defaultVersion <> ") "
-    inputVersionText <- TIO.getLine
-    let versionText = if T.null inputVersionText
-        then defaultVersion
-        else inputVersionText
-    TIO.putStrLn versionText
-
-    gitName <- readConfig "user.name"
-    name <- inputConfig "author.name: " gitName
-    TIO.putStrLn name
-
-    gitEmail <- readConfig "user.email"
-    email <- inputConfig "author.email: " gitEmail
-    TIO.putStrLn email
-
 readConfig :: T.Text -> IO (Maybe T.Text)
 readConfig cfg = do
-    let proc = (P.proc "git" ["config", T.unpack cfg]) { P.std_out = P.CreatePipe }
-    stream <- E.catch (do (_, s, _, _) <- P.createProcess proc
-                          return s)
-                      ((\_ -> return Nothing) :: E.IOException -> IO (Maybe a))
+    let proc = (P.proc "git" ["config", T.unpack cfg]) {
+                   P.std_out = P.CreatePipe
+               }
+    stream <- E.catch
+                  (do
+                      (_, s, _, _) <- P.createProcess proc
+                      return s
+                  )
+                  ((\ _ -> return Nothing) :: E.IOException -> IO (Maybe a))
     case stream of
         Just s -> do
             c <- TIO.hGetLine s
@@ -47,8 +77,47 @@ readConfig cfg = do
         Nothing -> return Nothing
 
 inputConfig :: T.Text -> Maybe T.Text -> IO T.Text
-inputConfig prompt gitName = do
-    let name = maybe T.empty (\n -> n) gitName
-    TIO.putStr $ prompt <>  maybe "" (\n -> "(" <> n <> ") ") gitName
-    authorName <- TIO.getLine
-    return $ if T.null authorName then name else authorName
+inputConfig prompt defaultValue = do
+    let emptyValue = M.fromMaybe T.empty defaultValue
+    TIO.putStr $ prompt <> maybe "" (\ n -> "(" <> n <> ") ") defaultValue
+    configValue <- TIO.getLine
+    return $ if T.null configValue then emptyValue else configValue
+
+parseSemVer :: T.Text -> IO SV.Version
+parseSemVer v =
+    case SV.fromText v of
+        Right r -> return r
+        Left _ -> SE.die $ T.unpack $ completeMessage v
+  where
+    errorMessage :: T.Text
+    errorMessage = "version expected to be fromatted as Semantic Versioning: "
+    completeMessage :: T.Text -> T.Text
+    completeMessage e' = errorMessage <> e'
+
+parseAuthor :: T.Text -> T.Text -> Author
+parseAuthor name' email' =
+    Author { name = name'
+           , email = case EV.validate (ENC.encodeUtf8 email') of
+                         Right e -> Just e
+                         Left _ -> Nothing
+           , uri = Nothing
+           }
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering
+    semVer <- inputConfig "verison: " $ Just defaultVersion
+    gitName <- readConfig "user.name"
+    name' <- inputConfig "author.name: " gitName
+    gitEmail <- readConfig "user.email"
+    email' <- inputConfig "author.email: " gitEmail
+    description' <- inputConfig "description: " $ Just ""
+    version' <- parseSemVer semVer
+    let author = parseAuthor name' email'
+        desc = if T.null description' then Just description' else Nothing
+        text = MW.encode Metadata { target = Quickstart
+                                  , authors = [author]
+                                  , version = version'
+                                  , description = desc
+                                  }
+    putStrLn $ T.unpack text
