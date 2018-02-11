8eb7d1e3d30a967f5d680eababd2b228fc2309ea
Wonwoo Choi <chwo9843@gmail.com>
2018-02-11 22:28:49 +0900

    Declare submodules in mod.rs and lib.rs

--- src/Nirum/Targets/Rust.hs
+++ src/Nirum/Targets/Rust.hs
@@ -9,25 +9,24 @@ module Nirum.Targets.Rust ( Rust
 import qualified Data.Map.Strict as M
 import qualified Data.SemVer as SV
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import Data.Text.Encoding (encodeUtf8)
 import Data.Text.Lazy (toStrict)
 import Data.Typeable (Typeable)
 
 import GHC.Exts (IsList (toList))
 
-import System.FilePath (joinPath, replaceExtension)
-
 import Text.Blaze.Renderer.Text
 import Text.Heterocephalus (compileText)
 
 import qualified Nirum.Constructs.Identifier as I
-import Nirum.Constructs.Module
 import Nirum.Constructs.ModulePath (ModulePath)
-import Nirum.Constructs.Name
 import Nirum.Constructs.TypeDeclaration
 import Nirum.Package.Metadata
 import qualified Nirum.Package.ModuleSet as MS
+import Nirum.Targets.Rust.Item
 import Nirum.Targets.Rust.Keyword
+import Nirum.Targets.Rust.ModuleTree
 import Nirum.TypeInstance.BoundModule
 
 data Rust = Rust { packageName :: T.Text
@@ -48,53 +47,41 @@ name = "#{ name' }"
 version = "#{ SV.toLazyText version' }"
 |]
 
-compileModule :: BoundModule Rust -> Code
-compileModule m =
-    toStrict $
-    renderMarkup [compileText|%{ forall (moduleName, members') <- enums }
-pub enum #{ toRustIdentifier I.toPascalCaseText $ facialName moduleName } {
-%{ forall EnumMember memberName _ <- members' }
-    #{ toRustIdentifier I.toPascalCaseText $ facialName memberName },
-%{ endforall }
-}
+buildPrologue :: RustModule -> TL.Text
+buildPrologue mod' =
+    renderMarkup [compileText|%{ forall child <- children mod' }
+pub mod #{ toRustIdentifier I.toCamelCaseText child };
 %{ endforall }
 |]
+
+buildBody :: Maybe (BoundModule Rust) -> TL.Text
+buildBody (Just m) =
+    TL.concat [renderItem i | i <- moduleTypes]
   where
     moduleTypes :: [TypeDeclaration]
     moduleTypes = toList $ boundTypes m
-    enums :: [(Name, [EnumMember])]
-    enums =
-        [ (moduleName, toList members')
-        | TypeDeclaration { typename = moduleName
-                          , type' = EnumType { members = members' }
-                          } <- moduleTypes
-        ]
+buildBody Nothing = TL.empty
 
 compilePackage' :: Package Rust
                 -> M.Map FilePath (Either CompileError' Code)
 compilePackage' package =
     M.fromList $
-        [ ( toFilename mp
-          , Right $ compileModule m
+        [ ( fileName
+          , Right $
+            toStrict $
+            TL.append (buildPrologue mod')
+                      (buildBody (mp >>= resolveWithModulePath))
           )
-        | (mp, _) <- modules'
-        , Just m <- [resolveBoundModule mp package]
+        | mod'@RustModule { filePath = fileName
+                          , modPath = mp
+                          } <- modules'
         ] ++
-        [ ("Cargo.toml", Right $ genCargoToml package)
-        , (joinPath ["src", "lib.rs"], Right "")
-        ]
+        [ ("Cargo.toml", Right $ genCargoToml package) ]
   where
-    convertModulePath :: ModulePath -> [FilePath]
-    convertModulePath mp =
-        "src" :
-        [ T.unpack (toRustIdentifier I.toSnakeCaseText i)
-        | i <- toList mp
-        ]
-    toFilename :: ModulePath -> FilePath
-    toFilename mp =
-        replaceExtension (joinPath $ convertModulePath mp) "rs"
-    modules' :: [(ModulePath, Module)]
-    modules' = MS.toAscList $ modules package
+    resolveWithModulePath :: ModulePath -> Maybe (BoundModule Rust)
+    resolveWithModulePath mp = resolveBoundModule mp package
+    modules' :: [RustModule]
+    modules' = buildRustModuleList [mp | (mp, _) <- MS.toAscList $ modules package]
 
 instance Target Rust where
     type CompileResult Rust = Code
