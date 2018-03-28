586a9be10fb67cd960cf7a485b91cf52326804fa
Hong Minhee <hong.minhee@gmail.com>
2018-03-28 22:51:27 +0900

    Elm: refactor functional tests of targets

--- app/test-targets.hs
+++ app/test-targets.hs
@@ -27,15 +27,38 @@ import qualified Data.Text as T
 import Text.InterpolatedString.Perl6 (qq)
 import Turtle
 
--- | Invoke a process.
-proc' :: T.Text -> [T.Text] -> IO ()
-proc' prog args =
-    catchIOError (procs prog args empty) $ \ ex ->
+-- | Invoke a process if the given 'program' exists.
+-- Invoke a fallback function if it does not exist.
+tryProc :: T.Text -> [T.Text] -> IO () -> IO ()
+tryProc program args fallback =
+    catchIOError (procs program args empty) $ \ ex ->
         if isDoesNotExistError ex
-        then do err ""
-                err [qq|Couldn't find executable: "$prog"|]
-                exit $ ExitFailure 1
+        then fallback
         else ioError ex
+
+-- | Invoke a process if the given 'program' exists.
+-- Print some error message and then terminate with an exit code 1
+-- if it does not exist.
+proc' :: T.Text -> [T.Text] -> IO ()
+proc' program args =
+    tryProc program args $ do
+        err ""
+        err [qq|Couldn't find executable: "$program".|]
+        exit $ ExitFailure 1
+
+tryProcEnv :: String -> T.Text -> [T.Text] -> IO () -> IO ()
+tryProcEnv envVar program args fallback = do
+    env <- lookupEnv envVar
+    let prog = maybe program T.pack env
+    tryProc prog args fallback
+
+procEnv :: String -> T.Text -> [T.Text] -> IO ()
+procEnv envVar program args =
+    tryProcEnv envVar program args $ do
+        err ""
+        err [qq|Couldn't find executable: "$program".|]
+        err [qq|(You can explicitly set $envVar to locate the "$program".)|]
+        exit $ ExitFailure 1
 
 -- | Invoke a 'nirum' compiler.
 nirum :: [T.Text] -> IO ()
@@ -43,12 +66,8 @@ nirum args = do
     nirumEnv <- lookupEnv "NIRUM"
     case nirumEnv of
         Just prog -> proc' (T.pack prog) args
-        Nothing -> catchIOError
-            (procs "stack" (["exec", "--", "nirum"] ++ args) empty) $ \ ex ->
-                if isDoesNotExistError ex
-                then proc' "nirum" args
-                else ioError ex
-                            
+        Nothing -> tryProc "stack" (["exec", "--", "nirum"] ++ args) $
+                           proc' "nirum" args
 
 -- | Run the IO operation only if it's run on CI.
 whenCi :: IO () -> IO ()
@@ -71,20 +90,17 @@ elm = withSystemTempDirectory "nirum-elm-test" $ \ tmpdir -> do
     let tmpdirT = T.pack tmpdir
     let tmpDirFP = fromString tmpdir :: Turtle.FilePath
     nirum ["-t", "elm", "-o", tmpdirT, "examples/"]
-    elmInstallEnv <- lookupEnv "ELM_INSTALL"
-    let elmInstall = maybe "elm-install" T.pack elmInstallEnv
-    elmMakeEnv <- lookupEnv "ELM_MAKE"
-    let elmMake = maybe "elm-make" T.pack elmMakeEnv
     back <- pwd
     cd tmpDirFP
-    proc' elmInstall []
-    proc' elmMake []
+    tryProcEnv "ELM_INSTALL" "elm-install" [] $
+        procEnv "ELM_PACKAGE" "elm-package" ["install"]
+    procEnv "ELM_MAKE" "elm-make" []
     cd back
 
 -- | Test Python target.
 python :: IO ()
 python = do
     toxEnv <- lookupEnv "TOX"
-    let tox = maybe "tox" T.pack toxEnv
-    whenCi $ proc' tox ["-e", "devruntime"]
-    proc' tox ["--skip-missing-interpreters"]
+    let tox = procEnv "TOX" "tox"
+    whenCi $ tox ["-e", "devruntime"]
+    tox ["--skip-missing-interpreters"]
