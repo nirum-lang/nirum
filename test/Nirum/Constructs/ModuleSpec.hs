60217b24167b2876910a371142ff05eb2791de85
Kang Hyojun <iam.kanghyojun@gmail.com>
2018-04-12 23:12:35 +0900

    Apply changed Import to ModuleSpec

--- test/Nirum/Constructs/ModuleSpec.hs
+++ test/Nirum/Constructs/ModuleSpec.hs
@@ -21,20 +21,27 @@ spec =
             pathT = TypeDeclaration "path" (Alias "text") (singleton docsAnno)
             offsetT =
                 TypeDeclaration "offset" (UnboxedType "float64") empty
-            decls = [ Import ["foo", "bar"] "baz" empty
-                    , Import ["foo", "bar"] "qux" empty
-                    , Import ["zzz"] "qqq" empty
-                    , Import ["zzz"] "ppp" empty
-                    , Import ["xyz"] "asdf" empty
+            decls = [ Import ["foo", "bar"] "baz" "baz" empty
+                    , Import ["foo", "bar"] "qux" "qux" empty
+                    , Import ["zzz"] "qqq" "qqq" empty
+                    , Import ["zzz"] "ppp" "ppp" empty
+                    , Import ["xyz"] "asdf" "asdf" empty
                     , pathT
                     , offsetT
                     ] :: DeclarationSet TypeDeclaration
+            decls2 = [ Import ["foo", "bar"] "qux" "baz" empty
+                     ] :: DeclarationSet TypeDeclaration
             mod1 = Module decls Nothing
             mod2 = Module decls $ Just "module level docs...\nblahblah"
+            mod3 = Module decls2 Nothing
         specify "imports" $ do
-            imports mod1 `shouldBe` [ (["foo", "bar"], ["baz", "qux"])
-                                    , (["xyz"], ["asdf"])
-                                    , (["zzz"], ["qqq", "ppp"])
+            imports mod1 `shouldBe` [ ( ["foo", "bar"]
+                                      , [("baz", "baz"), ("qux", "qux")]
+                                      )
+                                    , (["xyz"], [("asdf", "asdf")])
+                                    , ( ["zzz"]
+                                      , [("qqq", "qqq"), ("ppp", "ppp")]
+                                      )
                                     ]
             imports mod2 `shouldBe` imports mod1
         specify "toCode" $ do
@@ -57,4 +64,8 @@ type path = text;
 # path string
 
 unboxed offset (float64);
+|]
+            toCode mod3 `shouldBe` [q|import foo.bar (baz as qux);
+
+
 |]
